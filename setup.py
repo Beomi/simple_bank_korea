847fad44705b11161eda2afea071e0f11e7c1906
Beomi <latheledusjp@gmail.com>
2017-09-12 10:29:19 +0900

    Include assets files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='simple_bank_korea',
-      version='0.2.1',
+      version='0.2.2',
       url='https://github.com/beomi/simple_bank_korea',
       license='MIT',
       author='Junbum Lee',
@@ -14,5 +14,6 @@ setup(name='simple_bank_korea',
       zip_safe=False,
       setup_requires=['python-dateutil', 'requests', 'bs4', 'selenium', 'pillow'],
       install_requires=['python-dateutil', 'requests', 'bs4', 'selenium', 'pillow'],
-      download_url='https://github.com/beomi/simple_bank_korea/archive/0.2.1.tar.gz',
+      download_url='https://github.com/beomi/simple_bank_korea/archive/0.2.2.tar.gz',
+      include_package_data = True,
       )
