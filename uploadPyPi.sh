f5c7d665e23a4e6a2d767839e3a350226edeacd5
Junbum Lee <latheledusjp@gmail.com>
2020-06-04 16:00:46 +0900

    update pypi upload command

--- uploadPyPi.sh
+++ uploadPyPi.sh
@@ -1,4 +1,6 @@
 rm -rf build
 rm -rf dist
 rm -rf simple_bank_korea.egg-info
-python setup.py sdist upload -r pypi
+python setup.py sdist
+twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+twine upload dist/*
