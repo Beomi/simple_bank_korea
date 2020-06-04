f1f69bd8b1c14d4733339f570dc2a4ed8b378576
Junbum Lee <latheledusjp@gmail.com>
2020-06-04 16:37:21 +0900

    Update setup.py to use requirements.txt

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 from setuptools import setup, find_packages
 
+REQ_FILE = 'requirements.txt'
+VERSION = '0.2.15'
+
+def get_requires():
+    thisdir = os.path.dirname(__file__)
+    reqpath = os.path.join(thisdir, REQ_FILE)
+    return [line.rstrip('\n') for line in open(reqpath)]
+
 setup(name='simple_bank_korea',
-      version='0.2.14',
+      version=VERSION,
       url='https://github.com/beomi/simple_bank_korea',
       license='MIT',
       author='Junbum Lee',
       author_email='jun@beomi.net',
       description='Crawling Korea bank transactions',
       packages=find_packages(),
-      long_description="""\
-      Crawler with requests/bs4/selenium/PhantomJS for Korea Bank Transctions.
-      Currently supports Kookmin Bank""",
+      long_description=open('README.md', 'r', encoding="utf-8").read(),
+      long_description_content_type="text/markdown",
       zip_safe=False,
-      setup_requires=['python-dateutil', 'requests', 'bs4', 'selenium', 'pillow'],
-      install_requires=['python-dateutil', 'requests', 'bs4', 'selenium', 'pillow'],
-      download_url='https://github.com/beomi/simple_bank_korea/archive/0.2.14.tar.gz',
+      install_requires=get_requires(),
       include_package_data=True,
+      classifiers=[
+            "Programming Language :: Python :: 3",
+      ],
 )
